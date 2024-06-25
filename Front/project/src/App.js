941af63fdcf59fa99c1edc6805713d5224136ead
김민채 <alsco9707@gmail.com>
2024-06-25 22:08:42 +0900

    🐛 fix : 오류수정

--- Front/project/src/App.js
+++ Front/project/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Detail from 'routers/Detail'; // 파일 경로를 확인하세요
+
 function App() {
   return (
-    <div>
-      <h1 className="text-red-800 font-[godob]">App</h1>
-    </div>
+    <Router>
+      <div>
+        <h1 className="text-red-800 font-[godob]">App</h1>
+        <Routes>
+          <Route path="/detail" element={<Detail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
